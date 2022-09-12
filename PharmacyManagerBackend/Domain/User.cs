8ce534d34ac3b102ebe4a926e49552bcb3d51f65
federicocz <80722236+federicocz@users.noreply.github.com>
2022-09-12 18:01:15 -0300

    Update PharmacyManagerBackend/Domain/User.cs
    Co-authored-by: Cristhian Maciel <cristhian.maciel@mercadolibre.com>

--- PharmacyManagerBackend/Domain/User.cs
+++ PharmacyManagerBackend/Domain/User.cs
@@ -11,6 +11,6 @@ namespace Domain
         public string Email { get; set; }
         public string Address { get; set; }
         public string Password { get; set; }
-        public DateTime Registration { get; set; }
+        public DateTime RegistrationDate { get; set; }
     }
 }
