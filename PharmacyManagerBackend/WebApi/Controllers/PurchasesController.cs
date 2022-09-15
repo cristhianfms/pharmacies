960f0c326f73ad6b7354ca0e462055104c01148e
Cristhian Maciel <cristhian.maciel@mercadolibre.com>
2022-09-15 01:36:35 -0300

    RED: purchase controller created and its models

---
+++ PharmacyManagerBackend/WebApi/Controllers/PurchasesController.cs
@@ -0,0 +1,32 @@
+﻿using Domain;
+using Domain.Dtos;
+using IBusinessLogic;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using WebApi.Utils;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    
+    public class PurchasesController : ControllerBase
+    {
+        private IPurchaseLogic _purchaseLogic;
+
+        public PurchasesController(IPurchaseLogic purchaseLogic)
+        {
+            this._purchaseLogic = purchaseLogic;
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] PurchaseRequestModel purchaseRequestModel)
+        {
+            PurchaseDto purchase =  ModelsMapper.ToEntity(purchaseRequestModel);
+            PurchaseDto purhcaseCreated = _purchaseLogic.Create(purchase);
+            PurchaseResponseModel purchaseResponseModel = ModelsMapper.ToModel(purhcaseCreated);
+            
+            return Ok(purchaseResponseModel);
+        }
+    }
+}
