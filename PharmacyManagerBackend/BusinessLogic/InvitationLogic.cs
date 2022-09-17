97712ed6290e0e6160cb3e70dbd41d4d7a0917e7
Cristhian Maciel <cristhian.maciel@mercadolibre.com>
2022-09-17 19:48:42 -0300

    GREEN: created invitation without checks

--- PharmacyManagerBackend/BusinessLogic/InvitationLogic.cs
+++ PharmacyManagerBackend/BusinessLogic/InvitationLogic.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using IBusinessLogic;
 using Domain;
+using IDataAccess;
 
 namespace BusinessLogic
 {
     public class InvitationLogic : IInvitationLogic
     {
+        IBaseRepository<Invitation> _invitationRepository;
+
+        public InvitationLogic(IBaseRepository<Invitation> invitationRepository)
+        {
+            this._invitationRepository = invitationRepository;
+        }
+
         public virtual Invitation Create(Invitation invitation)
         {
-            return null;
+            Invitation createdInvitation = _invitationRepository.Create(invitation);
+
+            return createdInvitation;
         }
     }
 }
---
+++ PharmacyManagerBackend/IDataAccess/IBaseRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Domain;
+
+namespace IDataAccess
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        T Create(T entity);
+    }
+}
