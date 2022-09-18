b33f24bee47fc48a8fd4fd32f9a502d916b25222
Cristhian Maciel <cristhian.maciel@mercadolibre.com>
2022-09-17 21:00:10 -0300

    GREEN: added domain invitation test

---
+++ PharmacyManagerBackend/Domain.Test/InvitationTest.cs
@@ -0,0 +1,58 @@
+
+using Domain;
+using Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessLogic.Test.Dtos
+{
+    [TestClass]
+    public class InvitationTest
+    {
+        [TestMethod]
+        public void CheckValidInvitation()
+        {
+            Invitation invitation = new Invitation()
+            {
+                UserName = "Cris01",
+                Role = new Role()
+                {
+                    Name = "Employee"
+                }
+            };
+
+            invitation.CheckIsValid();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ValidationException))]
+        public void CheckEmptyUserNameThrowsException()
+        {
+            Invitation invitation = new Invitation()
+            {
+                UserName = "",
+                Role = new Role()
+                {
+                    Name = "Employee"
+                }
+            };
+
+            invitation.CheckIsValid();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ValidationException))]
+        public void CheckEmptyRoleNameThrowsException()
+        {
+            Invitation invitation = new Invitation()
+            {
+                UserName = "Cris01",
+                Role = new Role()
+                {
+                    Name = ""
+                }
+            };
+
+            invitation.CheckIsValid();
+        }
+    }
+}
