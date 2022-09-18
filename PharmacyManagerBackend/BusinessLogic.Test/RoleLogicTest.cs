ed01133e2b6ec7ee8a0e572f6c61c25991d925fc
Cristhian Maciel <cristhian.maciel@mercadolibre.com>
2022-09-18 20:24:46 -0300

    RED: get role by name

---
+++ PharmacyManagerBackend/BusinessLogic.Test/RoleLogicTest.cs
@@ -0,0 +1,38 @@
+using System;
+using Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace BusinessLogic.Test
+{
+    [TestClass]
+    public class RoleLogicTest
+    {
+        private RoleLogic _roleLogic;
+        private Mock<IRoleRepository> _roleRepository;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this._roleRepository = new Mock<IRoleRepository>(MockBehavior.Strict);
+            _roleLogic = new RoleLogic(this._roleLogic.Object);
+        }
+
+        [TestMethod]
+        public void GetRoleByNameOk()
+        {
+            string roleName = "Employee";
+            Role roleRepository = new Role()
+            {
+                Name = roleName
+            };
+
+            _roleRepository.Setup(m => m.GetFirst(It.IsAny<Func<Role, bool>>())).Returns(roleRepository);
+
+
+            Role roleReturned = _roleLogic.GetRoleByName(roleName);
+
+            Assert.AreEqual(roleRepository, roleReturned);
+        }
+    }
+}
