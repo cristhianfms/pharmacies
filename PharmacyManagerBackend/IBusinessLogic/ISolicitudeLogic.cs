5e4fcbbf51f1dafd23f8ab3827cd381e1f742c78
federicocz <fczarnievicz@gmail.com>
2022-09-19 17:10:31 -0300

    RED: Get solicitudes test controller

--- PharmacyManagerBackend/IBusinessLogic/ISolicitudeLogic.cs
+++ PharmacyManagerBackend/IBusinessLogic/ISolicitudeLogic.cs
@@ -1,4 +1,5 @@
 ﻿using Domain;
+using Domain.Dtos;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,5 +9,7 @@ namespace IBusinessLogic
     public interface ISolicitudeLogic
     {
         Solicitude Create(Solicitude solicitude);
+        List<Solicitude> GetSolicitudes(QuerySolicitudeDto querySolicitudeDto);
+        
     }
 }
--- PharmacyManagerBackend/WebApi.Test/SolicitudesControllerTest.cs
+++ PharmacyManagerBackend/WebApi.Test/SolicitudesControllerTest.cs
@@ -1,13 +1,16 @@
 ﻿using Domain;
+using Domain.Dtos;
 using IBusinessLogic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using WebApi.Controllers;
 using WebApi.Models;
+using WebApi.Utils;
 
 namespace WebApi.Test
 {
@@ -16,12 +19,56 @@ namespace WebApi.Test
     {
         private Mock<ISolicitudeLogic> _solicitudeLogicMock;
         private SolicitudesController _solicitudeApiController;
+        private User _userForTest;
+        private Solicitude _solicitudeForTest;
+
 
         [TestInitialize]
         public void InitTest()
         {
             _solicitudeLogicMock = new Mock<ISolicitudeLogic>(MockBehavior.Strict);
             _solicitudeApiController = new SolicitudesController(_solicitudeLogicMock.Object);
+
+            _userForTest = new User()
+            {
+                Id = 1,
+                UserName = "Usuario1",
+                Email = "ususario@user.com",
+                Address = "Cuareim 123",
+                Password = "Usuario+1",
+                Pharmacy = new Pharmacy()
+                {
+                    Name = "Pharmashop"
+                },
+                Role = new Role()
+                {
+                    Name = "Employee"
+                }
+            };
+            SolicitudeItem solicitudeItem3 = new SolicitudeItem()
+            {
+                DrugQuantity = 20,
+                DrugCode = "XF324",
+            };
+            SolicitudeItem solicitudeItem4 = new SolicitudeItem()
+            {
+                DrugQuantity = 3,
+                DrugCode = "AG123",
+            };
+
+            List<SolicitudeItem> solicitudeItems3and4 = new List<SolicitudeItem>()
+            {
+                solicitudeItem3, solicitudeItem4,
+            };
+
+            _solicitudeForTest = new Solicitude()
+            {
+                Id = 2,
+                State = State.PENDING,
+                Date = DateTime.Now,
+                Employee = _userForTest,
+                Items = solicitudeItems3and4
+            };
         }
 
 
@@ -45,8 +92,8 @@ namespace WebApi.Test
                 Id = 1,
                 State = State.PENDING,
                 Date = DateTime.Now,
-                Items = solicitudeItems
-
+                Items = solicitudeItems,
+                Employee = _userForTest
             };
 
             SolicitudeItemModel solicitudeItemModel = new SolicitudeItemModel()
@@ -54,8 +101,6 @@ namespace WebApi.Test
                 DrugCode = solicitudeItem.DrugCode,
                 DrugQuantity = solicitudeItem.DrugQuantity
             };
-
-
 
             List<SolicitudeItemModel> solicitudeItemModels = new List<SolicitudeItemModel>() 
             {
@@ -78,10 +123,65 @@ namespace WebApi.Test
             Assert.AreEqual(solicitude.Id, createdSolicitude.Id);
             Assert.AreEqual(solicitude.State, createdSolicitude.State);
             Assert.AreEqual(solicitude.Date, createdSolicitude.Date);
+            Assert.AreEqual(solicitude.Employee.UserName, createdSolicitude.EmployeeUserName);
             Assert.AreEqual(solicitude.Items[0].DrugCode, createdSolicitude.SolicitudeItems[0].DrugCode);
             Assert.AreEqual(solicitude.Items[0].DrugQuantity, createdSolicitude.SolicitudeItems[0].DrugQuantity);
             _solicitudeLogicMock.VerifyAll();
         }
 
+        [TestMethod]
+        public void GetAllSolicitudesOk()
+        {
+            SolicitudeItem solicitudeItem1 = new SolicitudeItem()
+            {
+                DrugQuantity = 20,
+                DrugCode = "XF324",
+            };
+            SolicitudeItem solicitudeItem2 = new SolicitudeItem()
+            {
+                DrugQuantity = 3,
+                DrugCode = "AG123",
+            };
+
+            List<SolicitudeItem> solicitudeItems1and2 = new List<SolicitudeItem>()
+            {
+                solicitudeItem1, solicitudeItem2,
+            };
+
+            Solicitude solicitude1ToReturn = new Solicitude()
+            {
+                Id = 1,
+                State = State.PENDING,
+                Date = DateTime.Now,
+                Employee = _userForTest,
+                Items = solicitudeItems1and2
+
+            };
+
+            List<Solicitude> solicitudesToReturn = new List<Solicitude>()
+            {
+                solicitude1ToReturn, _solicitudeForTest
+            };
+
+            QuerySolicitudeDto querySolicitudeDto = new QuerySolicitudeDto()
+            {
+
+            };
+            _solicitudeLogicMock.Setup(s => s.GetSolicitudes(querySolicitudeDto)).Returns(solicitudesToReturn);
+
+
+            var result = _solicitudeApiController.GetSolicitudes(querySolicitudeDto);
+            var okResult = result as OkObjectResult;
+            var solicitudes = okResult.Value as List<SolicitudeResponseModel>;
+            var solicitudesToReturnModels = ModelsMapper.ToModelList(solicitudesToReturn);
+
+            
+            Assert.AreEqual(solicitudesToReturnModels[0], solicitudes[0]);
+            Assert.AreEqual(solicitudesToReturnModels[1], solicitudes[1]);
+            Assert.AreEqual(solicitudesToReturnModels.Count, solicitudes.Count);
+            CollectionAssert.AreEqual(solicitudesToReturnModels, solicitudes);
+            _solicitudeLogicMock.VerifyAll();
+        }
+        
     }
 }
