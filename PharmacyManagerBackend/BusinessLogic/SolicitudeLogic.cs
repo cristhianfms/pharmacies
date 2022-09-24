1502e16f3c5a31c54755a63b1ce543e711d8ca39
federicocz <fczarnievicz@gmail.com>
2022-09-23 21:54:35 -0300

    GREEN: solicitude update ok, without validations

--- PharmacyManagerBackend/BusinessLogic/SolicitudeLogic.cs
+++ PharmacyManagerBackend/BusinessLogic/SolicitudeLogic.cs
@@ -40,26 +40,17 @@ namespace BusinessLogic
         public IEnumerable<Solicitude> GetSolicitudes(QuerySolicitudeDto querySolicitudeDto)
         {
             List<Solicitude> solicitudesToReturn = new List<Solicitude>();
-            //List<Solicitude> solicitudes = new List<Solicitude>();
             if (_context.CurrentUser.Role.Name.Equals("Employee"))
             {
-                //Ir aprovechando la lista y filtrando por cada cosa
                solicitudesToReturn = (List<Solicitude>)_solicitudeRepository.GetAll(
                      s => s.Employee.Id == _context.CurrentUser.Id);
-
                 if (querySolicitudeDto.State != null)
                 {
                     solicitudesToReturn.FindAll(s => s.State.Equals(querySolicitudeDto.State));
-                  //solicitudesToReturn = (List<Solicitude>)_solicitudeRepository.GetAll(
-                  //  s => s.Employee.Id == _context.CurrentUser.Id &&
-                  //  s.State.Equals(querySolicitudeDto.State));
                 }
                 if (querySolicitudeDto.DrugCode != null)
                 {
                     solicitudesToReturn.FindAll(s => s.Items.Any(x => x.DrugCode == querySolicitudeDto.DrugCode));
-                   // solicitudesToReturn = (List<Solicitude>)_solicitudeRepository.GetAll(
-                   //s => s.Employee.Id == _context.CurrentUser.Id &&
-                   //s.Items.Any(x => x.DrugCode == querySolicitudeDto.DrugCode));
                 }
                 if(querySolicitudeDto.DateFrom != null && querySolicitudeDto.DateTo != null)
                 {
@@ -67,12 +58,7 @@ namespace BusinessLogic
                     DateTime dateTo = toDateTime(querySolicitudeDto.DateTo);
                     validateDates(dateFrom, dateTo);
                     solicitudesToReturn.FindAll(s => s.Date >= dateFrom && s.Date <=dateTo);
-                    //solicitudesToReturn = (List<Solicitude>)_solicitudeRepository.GetAll(
-                    //s => s.Employee.Id == _context.CurrentUser.Id &&
-                    //s.Date >= dateFrom &&
-                    //s.Date <= dateTo);
                 } 
-
             } 
             else if (_context.CurrentUser.Role.Name.Equals("Owner"))
             {
@@ -94,43 +80,17 @@ namespace BusinessLogic
             }
         }
 
-        public Solicitude Update(int solicitudId, Solicitude solicitude)
-        {
-           return null;
-            
-            //Solicitude solicitudeToUpdate = _solicitudeRepository.GetFirst(solicitude.Id);
+        public Solicitude Update(int solicitudeId, Solicitude solicitude)
+        { 
+            Solicitude solicitudeToUpdate = _solicitudeRepository.GetFirst(s => s.Id == solicitudeId);
 
-            /*
-             Invitation invitation = getCreatedInvitation(invitationDto.Code);
-        checkInvitationUserName(invitation, invitationDto.UserName);
+            solicitudeToUpdate.State = solicitude.State;
 
-        User userToCreate = new User()
-        {
-            UserName = invitation.UserName,
-            Role = invitation.Role,
-            Email = invitationDto.Email,
-            Address = invitationDto.Address,
-            Password = invitationDto.Password,
-            RegistrationDate = DateTime.Now,
-            // TODO dependiento del tipo
-            //Pharmacy = invitation.Pharmacy
-        };
 
-        User createdUser = _userLogic.Create(userToCreate);
-        _invitationRepository.Delete(invitation);
 
-        InvitationDto invitationDtoToReturn = new InvitationDto()
-        {
-            UserName = userToCreate.UserName,
-            UserId = createdUser.Id,
-            RoleName = createdUser.Role.Name,
-            PharmacyName = createdUser.Pharmacy?.Name,
-            Email = createdUser.Email,
-            Address = createdUser.Password,
-        };
+            return solicitudeToUpdate;
 
-        return invitationDtoToReturn;
-             */
+                        
         }
     }
 }
