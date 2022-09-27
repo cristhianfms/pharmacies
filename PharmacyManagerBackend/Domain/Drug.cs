3e490f492eeecea40efa871a24781ecc3df2a4c2
Cristhian Maciel <cristhian.maciel@mercadolibre.com>
2022-09-27 20:51:46 -0300

    refex in permissions + permissions seed

--- PharmacyManagerBackend/Domain/Drug.cs
+++ PharmacyManagerBackend/Domain/Drug.cs
@@ -20,9 +20,8 @@ namespace Domain
         public double Price { get; set; }
         public int Stock { get; set; }
         public bool NeedsPrescription { get; set; }
-        public DrugInfo DrugInfo { get; set; }
         public int DrugInfoId { get; set; }
-
+        public DrugInfo DrugInfo { get; set; }
         public override bool Equals(object obj)
         {
             return this.DrugCode.Equals(((Drug)obj).DrugCode);
