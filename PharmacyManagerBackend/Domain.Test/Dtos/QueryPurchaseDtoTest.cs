3b3416b450ed9b8bc18961d9f084abc668801ff2
Cristhian Maciel <cristhian.maciel@mercadolibre.com>
2022-10-02 19:29:03 -0300

    RED: query querypurchasedto get parsed dates

---
+++ PharmacyManagerBackend/Domain.Test/Dtos/QueryPurchaseDtoTest.cs
@@ -0,0 +1,23 @@
+using Domain.Dtos;
+using Exceptions;
+
+namespace BusinessLogic.Test.Dtos;
+
+[TestClass]
+public class QueryPurchaseDtoTest
+{
+    [TestMethod]
+    public void DatesOk()
+    {
+        string dateFrom = "2022-09-01T00:00:00";
+        string dateTo = "2022-09-30T00:00:00";
+        QueryPurchaseDto queryPurchaseDto = new QueryPurchaseDto()
+        {
+            DateTo = dateTo,
+            DateFrom = dateFrom
+        };
+        
+        Assert.AreEqual(DateTime.Parse(dateFrom), queryPurchaseDto.GetParsedDateFrom());
+        Assert.AreEqual(DateTime.Parse(dateTo), queryPurchaseDto.GetParsedDateTo());
+    }
+}
