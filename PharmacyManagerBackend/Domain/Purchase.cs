4a854a278c00f619fc4aac11e74d57203667af63
Cristhian Maciel <cristhian.maciel@mercadolibre.com>
2022-10-01 23:04:23 -0300

    GREEN: purchase without items should throw a validation exception

--- PharmacyManagerBackend/Domain/Purchase.cs
+++ PharmacyManagerBackend/Domain/Purchase.cs
@@ -4,6 +4,7 @@ namespace Domain;
 public class Purchase
 {
     private string _userEmail;
+    private List<PurchaseItem> _items;
     public int Id { get; set; }
     public double TotalPrice { get; set; }
     public DateTime Date { get; set; }
@@ -21,6 +22,20 @@ public class Purchase
             }
         }
     }
-    
-    public List<PurchaseItem> Items { get; set; }
+
+    public List<PurchaseItem> Items
+    {
+        get
+        {
+            return _items;
+        }
+        set
+        {
+            if (value.Count < 1)
+            {
+                throw new ValidationException("purchase must contain at least one item");
+            }
+            _items = value;
+        }
+    }
 }
