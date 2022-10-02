4246520901a1089deefe72b48ad1be9dd610d22b
Cristhian Maciel <cristhian.maciel@mercadolibre.com>
2022-10-02 00:52:47 -0300

    RED: purchase without drug stock should fail

--- PharmacyManagerBackend/BusinessLogic.Test/PurchaseLogicTest.cs
+++ PharmacyManagerBackend/BusinessLogic.Test/PurchaseLogicTest.cs
@@ -10,115 +10,158 @@ namespace BusinessLogic.Test;
 [TestClass]
 public class PurchaseLogicTest
 {
-  private PurchaseLogic _purchaseLogic;
-  private Mock<IPurchaseRepository> _purchaseRepository;
-  private Mock<PharmacyLogic> _pharmacyLogic;
-  
-  [TestInitialize]
-  public void Initialize()
-  {
-    this._purchaseRepository = new Mock<IPurchaseRepository>(MockBehavior.Strict);
-    this._pharmacyLogic = new Mock<PharmacyLogic>(MockBehavior.Strict, null);
-    this._purchaseLogic = new PurchaseLogic(this._purchaseRepository.Object, this._pharmacyLogic.Object);
-  }
-  
-  [TestMethod]
-  public void CreatePurchaseOk()
-  {
-    Drug drug = new Drug()
+    private PurchaseLogic _purchaseLogic;
+    private Mock<IPurchaseRepository> _purchaseRepository;
+    private Mock<PharmacyLogic> _pharmacyLogic;
+
+    [TestInitialize]
+    public void Initialize()
     {
-      DrugCode = "A01"
-    };
-    Pharmacy pharmacy = new Pharmacy()
+        this._purchaseRepository = new Mock<IPurchaseRepository>(MockBehavior.Strict);
+        this._pharmacyLogic = new Mock<PharmacyLogic>(MockBehavior.Strict, null);
+        this._purchaseLogic = new PurchaseLogic(this._purchaseRepository.Object, this._pharmacyLogic.Object);
+    }
+
+    [TestMethod]
+    public void CreatePurchaseOk()
     {
-      Id = 1,
-      Name = "PharmacyName"
-    };
-    List<PurchaseItem> items = new List<PurchaseItem>()
+        Drug drug = new Drug()
+        {
+            DrugCode = "A01"
+        };
+        Pharmacy pharmacy = new Pharmacy()
+        {
+            Id = 1,
+            Name = "PharmacyName"
+        };
+        List<PurchaseItem> items = new List<PurchaseItem>()
+        {
+            new PurchaseItem()
+            {
+                Quantity = 1,
+                DrugId = 1,
+                Drug = drug
+            }
+        };
+        Purchase purchase = new Purchase()
+        {
+            UserEmail = "email@email.com",
+            Items = items,
+            Pharmacy = pharmacy
+        };
+        _pharmacyLogic.Setup(m => m.GetPharmacyByName(It.IsAny<string>())).Returns(pharmacy);
+        _purchaseRepository.Setup(m => m.Create(It.IsAny<Purchase>())).Returns(purchase);
+        _pharmacyLogic.Setup(m => m.GetDrug(It.IsAny<int>(), It.IsAny<string>())).Returns(drug);
+
+        _purchaseLogic.Create(purchase);
+
+        _pharmacyLogic.VerifyAll();
+        _purchaseRepository.VerifyAll();
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ValidationException))]
+    public void CreatePurchaseWithNotExistantPharmacyShouldFail()
     {
-      new PurchaseItem()
-      {
-        Quantity = 1,
-        DrugId = 1,
-        Drug = drug
-      }
-    };
-    Purchase purchase = new Purchase()
-    {
-      UserEmail = "email@email.com",
-      Items = items,
-      Pharmacy = pharmacy
-    };
-    _pharmacyLogic.Setup(m => m.GetPharmacyByName(It.IsAny<string>())).Returns(pharmacy);
-    _purchaseRepository.Setup(m => m.Create(It.IsAny<Purchase>())).Returns(purchase);
-    _pharmacyLogic.Setup(m => m.GetDrug(It.IsAny<int>(), It.IsAny<string>())).Returns(drug);
-    
-    _purchaseLogic.Create(purchase);
-    
-    _pharmacyLogic.VerifyAll();
-    _purchaseRepository.VerifyAll();
-  }
-  
-  [TestMethod]
-  [ExpectedException(typeof(ValidationException))]
-  public void CreatePurchaseWithNotExistantPharmacyShouldFail()
-  {
-    List<PurchaseItem> items = new List<PurchaseItem>()
-    {
-      new PurchaseItem()
-      {
-        Quantity = 1,
-        DrugId = 1
-      }
-    };
-    Purchase purchase = new Purchase()
-    {
-      UserEmail = "email@email.com",
-      Items = items,
-      Pharmacy = new Pharmacy()
-      {
-        Name = "PharmacyName"
-      }
-    };
-    _purchaseRepository.Setup(m => m.Create(It.IsAny<Purchase>())).Returns(purchase);
-    _pharmacyLogic.Setup(m => m.GetPharmacyByName(It.IsAny<string>())).Throws(new ResourceNotFoundException(""));
-    
-    _purchaseLogic.Create(purchase);
-  }
+        List<PurchaseItem> items = new List<PurchaseItem>()
+        {
+            new PurchaseItem()
+            {
+                Quantity = 1,
+                DrugId = 1
+            }
+        };
+        Purchase purchase = new Purchase()
+        {
+            UserEmail = "email@email.com",
+            Items = items,
+            Pharmacy = new Pharmacy()
+            {
+                Name = "PharmacyName"
+            }
+        };
+        _purchaseRepository.Setup(m => m.Create(It.IsAny<Purchase>())).Returns(purchase);
+        _pharmacyLogic.Setup(m => m.GetPharmacyByName(It.IsAny<string>())).Throws(new ResourceNotFoundException(""));
+
+        _purchaseLogic.Create(purchase);
+    }
 
 
-  [TestMethod]
-  [ExpectedException(typeof(ValidationException))]
-  public void CreatePurchaseWithNotExistentDrugShouldFail()
-  {
-    Pharmacy pharmacy = new Pharmacy()
+    [TestMethod]
+    [ExpectedException(typeof(ValidationException))]
+    public void CreatePurchaseWithNotExistentDrugShouldFail()
     {
-      Id = 1,
-      Name = "PharmacyName"
-    };
-    Drug drug = new Drug()
+        Pharmacy pharmacy = new Pharmacy()
+        {
+            Id = 1,
+            Name = "PharmacyName"
+        };
+        Drug drug = new Drug()
+        {
+            DrugCode = "A01"
+        };
+        List<PurchaseItem> items = new List<PurchaseItem>()
+        {
+            new PurchaseItem()
+            {
+                Quantity = 1,
+                DrugId = 1,
+                Drug = drug
+            }
+        };
+        Purchase purchase = new Purchase()
+        {
+            UserEmail = "email@email.com",
+            Items = items,
+            Pharmacy = pharmacy
+        };
+        _pharmacyLogic.Setup(m => m.GetPharmacyByName(It.IsAny<string>())).Returns(pharmacy);
+        _pharmacyLogic.Setup(m => m.GetDrug(It.IsAny<int>(), It.IsAny<string>()))
+            .Throws(new ResourceNotFoundException(""));
+        _purchaseRepository.Setup(m => m.Create(It.IsAny<Purchase>())).Returns(purchase);
+
+        _purchaseLogic.Create(purchase);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ValidationException))]
+    public void CreatePurchaseWithNotStockDrugShouldFail()
     {
-      DrugCode = "A01"
-    };
-    List<PurchaseItem> items = new List<PurchaseItem>()
-    {
-      new PurchaseItem()
-      {
-        Quantity = 1,
-        DrugId = 1,
-        Drug = drug
-      }
-    };
-    Purchase purchase = new Purchase()
-    {
-      UserEmail = "email@email.com",
-      Items = items,
-      Pharmacy = pharmacy
-    };
-    _pharmacyLogic.Setup(m => m.GetPharmacyByName(It.IsAny<string>())).Returns(pharmacy);
-    _pharmacyLogic.Setup(m => m.GetDrug(It.IsAny<int>(), It.IsAny<string>())).Throws(new ResourceNotFoundException(""));
-    _purchaseRepository.Setup(m => m.Create(It.IsAny<Purchase>())).Returns(purchase);
-    
-    _purchaseLogic.Create(purchase);
-  }
+        string drugCode = "A01";
+        string pharmacyName = "PharmacyName";
+        Pharmacy pharmacyRepository = new Pharmacy()
+        {
+            Id = 1,
+            Name = pharmacyName
+        };
+        Drug drug = new Drug()
+        {
+            DrugCode = drugCode,
+            Stock = 2
+        };
+        Purchase purchaseToCreate = new Purchase()
+        {
+            UserEmail = "email@email.com",
+            Pharmacy = new Pharmacy()
+            {
+                Name = pharmacyName
+            },
+            Items = new List<PurchaseItem>()
+            {
+                new PurchaseItem()
+                {
+                    Quantity = 3,
+                    Drug = new Drug()
+                    {
+                        DrugCode = drugCode
+                    }
+                }
+            }
+        };
+        _pharmacyLogic.Setup(m => m.GetPharmacyByName(It.IsAny<string>())).Returns(pharmacyRepository);
+        _purchaseRepository.Setup(m => m.Create(It.IsAny<Purchase>())).Returns(purchaseToCreate);
+        _pharmacyLogic.Setup(m => m.GetDrug(It.IsAny<int>(), It.IsAny<string>())).Returns(drug);
+
+        _purchaseLogic.Create(purchaseToCreate);
+    }
 }
