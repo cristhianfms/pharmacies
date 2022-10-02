bef0ba95596d7d09d63add2677b363e6db2533b4
Cristhian Maciel <cristhian.maciel@mercadolibre.com>
2022-10-02 19:29:37 -0300

    GREEN: query querypurchasedto get parsed dates

--- PharmacyManagerBackend/Domain/Dto/QueryPurchaseDto.cs
+++ PharmacyManagerBackend/Domain/Dto/QueryPurchaseDto.cs
@@ -1,7 +1,38 @@
+using Exceptions;
+
 namespace Domain.Dtos;
 
 public class QueryPurchaseDto
 {
-    public string DateFrom { get; set; }
-    public string DateTo { get; set; }
+    private DateTime? _dateFrom;
+    private DateTime? _dateTo;
+
+    public string? DateFrom
+    {
+        get { return _dateFrom.ToString(); }
+        set
+        {
+            _dateFrom = DateTime.Parse(value);
+        }
+    }
+
+    public string? DateTo
+    {
+        get { return _dateTo.ToString(); }
+        set
+        {
+            _dateTo = DateTime.Parse(value);
+        }
+        
+    }
+    
+    public DateTime? GetParsedDateFrom()
+    {
+        return _dateFrom;
+    }
+    
+    public DateTime? GetParsedDateTo()
+    {
+        return _dateTo;
+    }
 }
