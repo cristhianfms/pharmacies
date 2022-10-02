842bccf756e7524781470b6ffc82c418c4231cf8
Cristhian Maciel <cristhian.maciel@mercadolibre.com>
2022-10-02 00:03:47 -0300

    RED: create purchase with not existant pharmacy

--- PharmacyManagerBackend/BusinessLogic.Test/PurchaseLogicTest.cs
+++ PharmacyManagerBackend/BusinessLogic.Test/PurchaseLogicTest.cs
@@ -1,5 +1,6 @@
 using Domain;
 using Domain.Dtos;
+using Exceptions;
 using IDataAccess;
 using Moq;
 
@@ -33,6 +34,32 @@ public class PurchaseLogicTest
     {
       UserEmail = "email@email.com",
       Items = items
+    };
+    _purchaseRepository.Setup(m => m.Create(It.IsAny<Purchase>())).Returns(purchase);
+    
+    _purchaseLogic.Create(purchase);
+  }
+  
+  [TestMethod]
+  [ExpectedException(typeof(ValidationException))]
+  public void CreatePurchaseWithNotExistantPharmacyShouldFail()
+  {
+    List<PurchaseItem> items = new List<PurchaseItem>()
+    {
+      new PurchaseItem()
+      {
+        Quantity = 1,
+        DrugId = 1
+      }
+    };
+    Purchase purchase = new Purchase()
+    {
+      UserEmail = "email@email.com",
+      Items = items,
+      Pharmacy = new Pharmacy()
+      {
+        Name = "PharmacyName"
+      }
     };
     _purchaseRepository.Setup(m => m.Create(It.IsAny<Purchase>())).Returns(purchase);
     
