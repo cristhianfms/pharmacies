15eb12658b7a62f25dc4e9335159452ca21e4e8c
Cristhian Maciel <cristhian.maciel@mercadolibre.com>
2022-10-01 22:59:20 -0300

    GREEN: purchase without user email should throw an exception

--- PharmacyManagerBackend/Domain/Purchase.cs
+++ PharmacyManagerBackend/Domain/Purchase.cs
@@ -1,9 +1,26 @@
+using Exceptions;
+
 namespace Domain;
 public class Purchase
 {
+    private string _userEmail;
     public int Id { get; set; }
     public double TotalPrice { get; set; }
     public DateTime Date { get; set; }
-    public string UserEmail { get; set; }
+    public string UserEmail
+    {
+        get
+        {
+            return _userEmail;
+        }
+        set
+        {
+            if (String.IsNullOrEmpty(value))
+            {
+                throw new ValidationException("user email can not be null");
+            }
+        }
+    }
+    
     public List<PurchaseItem> Items { get; set; }
 }
