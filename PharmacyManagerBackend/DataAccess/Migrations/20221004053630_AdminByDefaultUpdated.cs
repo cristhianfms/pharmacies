f483e6311acea19ddfc261551953736af025711f
Cristhian Maciel <maciel.cristhian@gmail.com>
2022-10-04 02:37:09 -0300

    migration for admin user udpated

---
+++ PharmacyManagerBackend/DataAccess/Migrations/20221004053630_AdminByDefaultUpdated.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class AdminByDefaultUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PermissionSet",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Endpoint",
+                value: "GET/api/drugs*");
+
+            migrationBuilder.UpdateData(
+                table: "UserSet",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Address", "Password" },
+                values: new object[] { "dirección", "admin1234-" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PermissionSet",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Endpoint",
+                value: "GET/api/drugs/*");
+
+            migrationBuilder.UpdateData(
+                table: "UserSet",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Address", "Password" },
+                values: new object[] { "", "admin1234" });
+        }
+    }
+}
