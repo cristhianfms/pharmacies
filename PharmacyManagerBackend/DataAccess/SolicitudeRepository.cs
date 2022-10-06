e31046c7d943c2d38c5fd0972d680d198c2eba25
NachoOlivera <nacho.oliveram@gmail.com>
2022-10-06 20:48:45 -0300

    Added documents

--- PharmacyManagerBackend/DataAccess/SolicitudeRepository.cs
+++ PharmacyManagerBackend/DataAccess/SolicitudeRepository.cs
@@ -1,32 +1,39 @@
-﻿using Domain;
-using Exceptions;
-using IDataAccess;
-using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain;
+using Exceptions;
+using IDataAccess;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess
 {
-    public class SolicitudeRepository : BaseRepository<Solicitude>, ISolicitudeRepository
+    public class
+    SolicitudeRepository
+    : BaseRepository<Solicitude>, ISolicitudeRepository
     {
         private readonly DbContext _context;
+
         private readonly DbSet<Solicitude> _table;
-        public SolicitudeRepository(DbContext dbContext) : base(dbContext)
+
+        public SolicitudeRepository(DbContext dbContext) :
+            base(dbContext)
         {
             this._context = dbContext;
             this._table = _context.Set<Solicitude>();
         }
 
-        public override IEnumerable <Solicitude> GetAll(Func<Solicitude, bool> expresion = null)
+        public override IEnumerable<Solicitude> GetAll(Func<Solicitude, bool> expresion = null)
         {
-            IEnumerable<Solicitude> entities = this._table.
-            Include(s => s.Items).
-            Include(s => s.Employee).
-            Where(expresion);
-            List <Solicitude> entitiesToReturn;
+            IEnumerable<Solicitude> entities =
+                this
+                    ._table
+                    .Include(s => s.Items)
+                    .Include(s => s.Employee)
+                    .Where(expresion);
+            List<Solicitude> entitiesToReturn;
             try
             {
                 entitiesToReturn = entities.Where(expresion).ToList();
@@ -41,10 +48,12 @@ namespace DataAccess
 
         public override Solicitude GetFirst(Func<Solicitude, bool> expresion)
         {
-            IEnumerable<Solicitude> entities = this._table
-                .Include(s => s.Items)
-                .Include(s => s.Employee)
-                .Where(expresion);
+            IEnumerable<Solicitude> entities =
+                this
+                    ._table
+                    .Include(s => s.Items)
+                    .Include(s => s.Employee)
+                    .Where(expresion);
             Solicitude entityToReturn;
             try
             {
