476d1c9b64d4b6be363ce8d544a3a06b98e17759
Cristhian Maciel <maciel.cristhian@gmail.com>
2022-11-06 02:07:59 -0300

    purchase report service

--- PharmacyManagerFrontend/pharmacy-app/src/app/owner/pages/purchase-report/purchase-report.component.ts
+++ PharmacyManagerFrontend/pharmacy-app/src/app/owner/pages/purchase-report/purchase-report.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {DrugsService} from "../../../services/drugs.service";
+import {Drug} from "../../../models/drug.model";
+import {PurchasesService} from "../../../services/purchases.service";
+import {Purchase} from "../../../models/purchase.model";
+import {PurchaseList} from "../../../models/purchase-list.model";
 
 @Component({
   selector: 'app-purchase-report',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PurchaseReportComponent implements OnInit {
 
-  constructor() { }
+  dateFrom: string = ''
+  dateTo: string = ''
+  purchaseList: any = null
+  totalAmount: number = 0
+  getStatus: 'loading' | 'success' | 'error' | null = null
+  errorMessage: string = ''
+
+  constructor(private purchasesServices: PurchasesService) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    this.getStatus = 'loading'
+    this.purchasesServices.getPurchasesReport(this.dateFrom, this.dateTo).subscribe({
+          next: this.handleGetResponse.bind(this),
+          error: this.handleError.bind(this)
+        }
+    )
+  }
+
+  handleGetResponse(data: any){
+    this.purchaseList = data.purchases;
+    this.totalAmount = data.totalPrice
+    this.getStatus = 'success'
+  }
+
+  handleError(error: any){
+    this.getStatus = 'error'
+    this.purchaseList = null
+    this.errorMessage = error.error.message
+  }
 }
