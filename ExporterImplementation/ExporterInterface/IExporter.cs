16f111a7515894a767a6b1238f2902c747eb2daa
Cristhian Maciel <maciel.cristhian@gmail.com>
2022-11-06 00:23:56 -0300

    new solution for exporter implementation

---
+++ ExporterImplementation/ExporterInterface/IExporter.cs
@@ -0,0 +1,11 @@
+﻿using ExporterDomain;
+using ExporterDomain.Dto;
+
+namespace ExporterInterface;
+
+public interface IExporter
+{
+      string GetName();
+      List<ExportPropertyDto> GetProperties();
+      void ExportDrugs(List<Drug> drugs, List<ExportPropertyDto> props);
+}
---
+++ ExporterImplementation/JsonExporter/JsonExporterImp.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using ExporterDomain;
+using ExporterDomain.Dto;
+using ExporterInterface;
+
+namespace JsonExporter;
+
+public class JsonExporterImp : IExporter
+{
+    public string GetName()
+    {
+        return "json-exporter";
+    }
+
+    public List<ExportPropertyDto> GetProperties()
+    {
+        return new List<ExportPropertyDto>()
+        {
+            new ExportPropertyDto()
+            {
+                Type = "string",
+                Key = "FileName",
+            }
+        };
+    }
+
+    public void ExportDrugs(List<Drug> drugs, List<ExportPropertyDto> props)
+    {
+        string path = props.Find(p => p.Key == "FileName").Value;
+        string json = JsonSerializer.Serialize(drugs);
+        File.WriteAllText(path, json);
+    }
+}
